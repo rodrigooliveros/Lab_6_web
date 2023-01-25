36fbefc77f012f39dcc493664c0c4fd08759b5fb
rodrigooliveros <A01329696@tec.mx>
2023-01-24 20:14:46 -0600

    2 intento

--- script.js
+++ script.js
@@ -25,15 +25,34 @@ array.forEach(element => {
 
 // La logica del click de cada boton para hacer la llamda al API
 $("#animal-buttons").on("click", ".animal-button", function() {
-
+    //alert("hola")
+    var rating = "all"
+    var liga = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-type") + "&api_key=BZeg7AThXsLFDGjj3iciLyE703JRclB7&limit=20&rating=" + rating + "&lang=en";
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", liga);
+    xhr.send();
+    xhr.onload = function(){
+        var respuesta = JSON.parse(xhr.response);
+        console.log(respuesta);
+        respuesta.data.forEach(element => {
+            var a = $("<img>");
+            a.attr("src",element.images.fixed_height.url)
+            a.addClass("animal-image")
+            $("#animals").append(a);
+        });
+    }
     $("#animals").empty();
-
 })
 
 
 // La lógica del click de cada imagen para "intercambiar las urls"
 $("#animals").on("click", ".animal-image", function(){
-
+    var src = $(this).attr("src");
+    if(src.includes("_s")){
+        $(this).attr("src",src.replace("_s",""));
+    }else{
+        $(this).attr("src",src.replace(".gif","_s.gif"));
+    }
 
 })
 
@@ -41,7 +60,12 @@ $("#animals").on("click", ".animal-image", function(){
 // La lógica del formulario para agregar mas botones a la lista
 $("#add-animal").on("click", function(e) {
     e.preventDefault();
-
+    var newAnimal = $("input").eq(0).val();
+    if(newAnimal.length > 2){
+        animals.push(newAnimal);
+    }
+    populateButtons(animals);
+    $("input").eq(0).val("");
 })
 
 
